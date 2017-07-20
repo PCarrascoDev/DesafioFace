fa5427091ae92f220175520101d84d1efac536bc
Pedro <pedro.carrascoc@mail.udp.cl>
2017-07-20 19:55:31 -0400

    Fifth Commit Modified Splash Activity, Login, LoginActivity, content_main Added DevelopersFragment, fragment_developers

--- app/src/main/java/com/desafiolatam/desafioface/views/main/DevelopersFragment.java
+++ app/src/main/java/com/desafiolatam/desafioface/views/main/DevelopersFragment.java
@@ -2,7 +2,11 @@ package com.desafiolatam.desafioface.views.main;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +18,7 @@ import com.desafiolatam.desafioface.R;
  */
 public class DevelopersFragment extends Fragment {
 
+    private SwipeRefreshLayout reload;
 
     public DevelopersFragment() {
         // Required empty public constructor
@@ -27,4 +32,18 @@ public class DevelopersFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_developers, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        reload = (SwipeRefreshLayout) view.findViewById(R.id.reloadSrl);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.developersRv);
+
+        recyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        linearLayoutManager.findLastCompletelyVisibleItemPosition();
+    }
 }
